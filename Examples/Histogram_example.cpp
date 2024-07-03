0d736b1fae3380345f5a20b959097933b7fa8507
shnarda <shnarda@outlook.com>
2024-07-03 21:14:30 +0300

    Variable names and line comments have been refactored to be more clear.

--- Examples/Histogram_example.cpp
+++ Examples/Histogram_example.cpp
@@ -1,22 +1,22 @@
-//#include <iostream>
-//#include "plotelementbase.h"
 #include "opencv2/highgui.hpp"
 #include "histogram.h"
-#include <paintertools.h>
 
 int main()
 {
-    //Prepare the histogram data
-    cv::Mat dummyData(200, 100, CV_32F);
-    cv::randn(dummyData, 4, 20);
+    //Prepare the normal distributed histogram data and pass it on a Histogram object
+    cv::Mat gaussian(200, 100, CV_32F);
+    cv::randn(gaussian, 4, 20);
+    Histogram hist{gaussian};
 
-    Histogram test{dummyData};
+    //Set some of the available properties
+    hist.setText(TextField::Title, "Histogram Example", 1, PainterConstants::blue);
+    hist.setText(TextField::XAxis, "Gaussian Distribution X-Axis", 1, PainterConstants::red);
+    hist.setCanvasSize(cv::Size{450, 450});
 
-    test.setText(TextField::Title, "Histogram Example", 1, PainterConstants::blue);
-    test.setText(TextField::XAxis, "Gaussian Distribution X-Axis", 1, PainterConstants::red);
-    //test.setRanges(AxisRange{0, 60}, {});
-    cv::Mat out = test.generate();
+    //Generate the histogram canvas and retrieve it
+    cv::Mat histogramCanvas = hist.generate();
 
-    cv::imshow("test", out);
+    //Display the output canvas
+    cv::imshow("Histogram Example", histogramCanvas);
     cv::waitKey(0);
 }
